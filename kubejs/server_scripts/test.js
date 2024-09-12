5303bd2eb68319ddb473d53c6cd8f894eeefc766
Andrew Coffman <andrewcoffman679@gmail.com>
2024-09-12 12:52:46 +0000

    fix reference to molten_iron tag

--- kubejs/server_scripts/test.js
+++ kubejs/server_scripts/test.js
@@ -1,8 +1,8 @@
 ServerEvents.recipes(e => {
-    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Fluid.of('molten_metals:molten_iron',), '#tconstruct:casts/single_use/pick_head'])
-    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Fluid.of('#c:molten_iron',), '#tconstruct:casts/single_use/pick_head'])
-    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:copper"}'), [Fluid.of('molten_metals:molten_copper',), '#tconstruct:casts/single_use/pick_head'])
-    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:gold"}'), [Fluid.of('molten_metals:molten_gold',), '#tconstruct:casts/single_use/pick_head'])
+    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Fluid.of('molten_metals:molten_iron',200), '#tconstruct:casts/single_use/pick_head'])
+    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Ingredient.of('#c:molten_iron',200), '#tconstruct:casts/single_use/pick_head'])
+    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:copper"}'), [Fluid.of('molten_metals:molten_copper'), '#tconstruct:casts/single_use/pick_head'])
+    e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:gold"}'), [Fluid.of('molten_metals:molten_gold'), '#tconstruct:casts/single_use/pick_head'])
   })
 // Listen to item tag event
 ServerEvents.tags('fluids', event => {
