755d7a52eb53b61d939c06e25bf77563b725cdc4
Andrew Coffman <andrewcoffman679@gmail.com>
2024-09-12 19:52:01 +0000

    add recipies for pickaxe and axe heads

--- kubejs/server_scripts/test.js
+++ kubejs/server_scripts/test.js
@@ -1,12 +1,18 @@
+var molten_iron;
+var molten_gold;
+var molten_copper;
 // Listen to item tag event
 ServerEvents.tags('fluid', event => {
   // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  event.add('c:molten_iron', 'molten_metals:molten_iron')
-  event.add('tconstruct:molten_iron', 'molten_metals:molten_iron')
-  event.add('c:molten_copper', 'molten_metals:molten_copper')
-  event.add('tconstruct:molten_copper', 'molten_metals:molten_copper')
-  event.add('c:molten_gold', 'molten_metals:molten_gold')
-  event.add('tconstruct:molten_gold', 'molten_metals:molten_gold')
+  event.add('c:molten_iron', 'molten_metals:molten_iron');
+  event.add('tconstruct:molten_iron', 'molten_metals:molten_iron');
+  event.add('c:molten_copper', 'molten_metals:molten_copper');
+  event.add('tconstruct:molten_copper', 'molten_metals:molten_copper');
+  event.add('c:molten_gold', 'molten_metals:molten_gold');
+  event.add('tconstruct:molten_gold', 'molten_metals:molten_gold');
+  molten_iron = e.get("c:molten_iron").getObjectIds();
+  molten_gold = e.get("c:molten_gold").getObjectIds();
+  molten_copper = e.get("c:molten_copper").getObjectIds();
 })
 /*
 ServerEvents.tags('item', event => {
@@ -17,11 +23,20 @@ ServerEvents.tags('item', event => {
 */
 
 ServerEvents.recipes(e => {
-  const molten_iron = e.get("c:molten_iron").getObjectIds()
-  for(const fluid of molten_iron){
-    console.info(fluid)
+  let casting = (output, fluidTag, fluidAmount, mold) => {
+    for(const fluid of fluidTag){
+      console.info(fluid);
+      e.recipes.create.filling(output, [Fluid.of(fluid, FluidAmount), mold]);
+    }
   }
-  e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Fluid.of('molten_metals:molten_iron', FluidAmounts.INGOT), '#tconstruct:casts/single_use/pick_head'])
-  e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:copper"}'), [Fluid.of('molten_metals:molten_copper'), '#tconstruct:casts/single_use/pick_head'])
-  e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:gold"}'), [Fluid.of('molten_metals:molten_gold'), '#tconstruct:casts/single_use/pick_head'])
+  casting(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), molten_iron, FluidAmounts.INGOT, '#tconstruct:casts/single_use/pick_head');
+  casting(Item.of('tconstruct:pick_head', '{Material:"tconstruct:copper"}'), molten_copper, FluidAmounts.INGOT, '#tconstruct:casts/single_use/pick_head');
+
+  casting(Item.of('tconstruct:small_axe_head', '{Material:"tconstruct:iron"}'), molten_iron, FluidAmounts.INGOT, '#tconstruct:casts/single_use/small_axe_head');
+  casting(Item.of('tconstruct:small_axe_head', '{Material:"tconstruct:copper"}'), molten_copper, FluidAmounts.INGOT, '#tconstruct:casts/single_use/small_axe_head');
+
+  casting(Item.of('tconstruct:broad_axe_head', '{Material:"tconstruct:iron"}'), molten_iron, FluidAmounts.INGOT, '#tconstruct:casts/single_use/broad_axe_head');
+  casting(Item.of('tconstruct:broad_axe_head', '{Material:"tconstruct:copper"}'), molten_copper, FluidAmounts.INGOT, '#tconstruct:casts/single_use/broad_axe_head');
+
+  e.recipes.create.compacting(['#minecraft:saplings', 'tconstruct:pick_head'], ['sand', 'tconstruct:pick_head']).keepIngredient('tconstruct:pick_head');
 })
