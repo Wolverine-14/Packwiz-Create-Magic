fd233a7c035b3567ae596a6c6caf08d5cf8588aa
Andrew Coffman <andrewcoffman679@gmail.com>
2024-09-12 13:20:56 +0000

    update molten_metals item tags as well

--- kubejs/server_scripts/test.js
+++ kubejs/server_scripts/test.js
@@ -1,11 +1,19 @@
+// Listen to item tag event
+ServerEvents.tags('fluids', event => {
+  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+  event.add('c:molten_iron', 'molten_metals:molten_iron')
+  event.add('tconstruct:molten_iron', 'molten_metals:molten_iron')
+})
+
+ServerEvents.tags('item', event => {
+  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
+  event.add('c:molten_iron', 'molten_metals:molten_iron')
+  event.add('tconstruct:molten_iron', 'molten_metals:molten_iron')
+})
+
 ServerEvents.recipes(e => {
     e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Fluid.of('molten_metals:molten_iron',200), '#tconstruct:casts/single_use/pick_head'])
     e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:iron"}'), [Ingredient.of('#c:molten_iron',200), '#tconstruct:casts/single_use/pick_head'])
     e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:copper"}'), [Fluid.of('molten_metals:molten_copper'), '#tconstruct:casts/single_use/pick_head'])
     e.recipes.create.filling(Item.of('tconstruct:pick_head', '{Material:"tconstruct:gold"}'), [Fluid.of('molten_metals:molten_gold'), '#tconstruct:casts/single_use/pick_head'])
   })
-// Listen to item tag event
-ServerEvents.tags('fluids', event => {
-  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  event.add('c:molten_iron', 'molten_metals:molten_iron')
-})
