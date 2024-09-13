57d3e7d677bbc827d3793e51dedf40027ffe7898
Andrew Coffman <andrewcoffman679@gmail.com>
2024-09-13 13:00:52 +0000

    try using keep ingredient on compacting recipie

--- kubejs/server_scripts/test.js
+++ kubejs/server_scripts/test.js
@@ -1,6 +1,7 @@
 var molten_iron;
 var molten_gold;
 var molten_copper;
+var molds;
 const tcMaterials = ['cobalt','darkthread','queens_slime','hepatizon','manyullyn','blazing_bone','enderslime','wood','rock','flint','bone','copper','bamboo','chorus','leather','string','phantom','rotten_flesh','iron','seared_stone','bloodbone','slimewood','necrotic_bone','scorched_stone','whitestone','ender_pearl','glass','gold','slimesteel','amethyst_bronze','nahuatl','pig_iron','rose_gold','plated_slimewood']
 
 // Listen to item tag event
@@ -15,6 +16,7 @@ ServerEvents.tags('fluid', event => {
   molten_iron = event.get("c:molten_iron").getObjectIds();
   molten_gold = event.get("c:molten_gold").getObjectIds();
   molten_copper = event.get("c:molten_copper").getObjectIds();
+  molds = event.get('tconstruct:casts/single_use').getObjectIds();
 })
 
 ServerEvents.recipes(e => {
@@ -32,15 +34,24 @@ ServerEvents.recipes(e => {
   casting(Item.of('tconstruct:broad_axe_head', '{Material:"tconstruct:iron"}'), molten_iron, FluidAmounts.INGOT*8, '#tconstruct:casts/single_use/broad_axe_head');
   casting(Item.of('tconstruct:broad_axe_head', '{Material:"tconstruct:copper"}'), molten_copper, FluidAmounts.INGOT*8, '#tconstruct:casts/single_use/broad_axe_head');
 
-  for(const mold of Item.getTypeList()){
-    if(Item.of(mold).hasTag('tconstruct:casts/single_use')){
-      for(const material of tcMaterials){
-        if(mold.includes('_red_sand_cast')){
-          e.recipes.create.compacting([mold, Item.of(mold.replace('_red_sand_cast',''), '{Material:"tconstruct:' + material + '"}')], ['red_sand', Item.of(mold.replace('_red_sand_cast',''), '{Material:"tconstruct:' + material + '"}')]);
-        } else {
-          e.recipes.create.compacting([mold, Item.of(mold.replace('_sand_cast',''), '{Material:"tconstruct:' + material + '"}')], ['sand', Item.of(mold.replace('_sand_cast',''), '{Material:"tconstruct:' + material + '"}')]);
-        }
+  Ingredient.registerCustomIngredientAction("apply_enchantment", (itemstack, index, inventory) => {
+    let enchantment = inventory.get(inventory.find(Item.of("minecraft:enchanted_book").ignoreNBT())).nbt;
+    if (itemstack.nbt == null)
+        itemstack.nbt = {}
+    itemstack.nbt = itemstack.nbt.merge({ Enchantments: enchantment.get("StoredEnchantments") })
+    return itemstack;
+  })
+
+  for(const mold of molds){
+    /*
+    for(const material of tcMaterials){
+      if(mold.includes('_red_sand_cast')){
+        e.recipes.create.compacting([mold, Item.of(mold.replace('_red_sand_cast',''), '{Material:"tconstruct:' + material + '"}')], ['red_sand', Item.withNBT(mold.replace('_red_sand_cast',''), '{Material:"tconstruct:' + material + '"}')]);
+      } else {
+        e.recipes.create.compacting([mold, Item.of(mold.replace('_sand_cast',''), '{Material:"tconstruct:' + material + '"}')], ['sand', Item.withNBT(mold.replace('_sand_cast',''), '{Material:"tconstruct:' + material + '"}')]);
       }
     }
+    */
+    e.recipes.create.compacting(mold, ['sand', mold.replace('_sand_cast','')]).keepIngredient(mold.replace('_sand_cast',''));
   }
 })
